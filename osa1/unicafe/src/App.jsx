227afeae3886e607e69ca6009993bdc0690f421d
Henri Heilala <henri.heilala@nitor.com>
2023-01-30 12:33:10 +0200

    complete 1.8

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -24,20 +24,36 @@ const StatRow = ({text, interactions}) => {
   )
 }
   
+const Statistics = ({texts, values}) => {
+
+  return(
+    <div>
+      <Header text={texts.header2} />
+      <StatRow text={texts.badText} interactions={values.bad}/>
+      <StatRow text={texts.neutralText} interactions={values.neutral}/>
+      <StatRow text={texts.goodText} interactions={values.good}/>
+      <StatRow text={texts.allText} interactions={values.bad+values.neutral+values.good}/>
+      <StatRow text={texts.averageText} interactions={values.average}/>
+    </div>
+  )
+}
+
 const App = (props) => {
   const [badValue, setBadValue] = useState(0)
   const [neutralValue, setNeutralValue] = useState(0)
   const [goodValue, setGoodValue] = useState(0)
   const [averageValue, setAverageValue] = useState(0)
 
-  const header1 = "Give Feedback!"
-  const header2 = "Statistics"
-  const badText = "Bad"
-  const neutralText = "Neutral"
-  const goodText = "Good! :)"
-  const averageText = "Average of reviews"
-  const allText = "All"
-  const positiveText = "Positive"
+  const textData = {
+  "header1":"Give Feedback!",
+  "badText":"Bad",
+  "neutralText":"Neutral",
+  "goodText" :"Good! :)",
+  "header2":"Statistics",
+  "averageText":"Average of reviews",
+  "allText":"All",
+  "positiveText":"Positive"
+  }
 
   const badIncrease = () => {
     console.log("increasing bad value by 1")
@@ -63,19 +79,13 @@ const App = (props) => {
   }
 
   return (
-    <>
-      <Header text={header1}/>
-      <Button text={badText} handleClick={badIncrease} />
-      <Button text={neutralText} handleClick={neutralIncrease} />
-      <Button text={goodText} handleClick={goodIncrease} />
-      <Header text={header2} />
-      <StatRow text={badText} interactions={badValue}/>
-      <StatRow text={neutralText} interactions={neutralValue}/>
-      <StatRow text={goodText} interactions={goodValue}/>
-      <StatRow text={allText} interactions={badValue+neutralValue+goodValue}/>
-      <StatRow text={averageText} interactions={averageValue}/>
-      <StatRow text={positiveText} interactions={(goodValue/(badValue+neutralValue+goodValue)*100)+'%'}/>
-    </>
+    <div>
+      <Header text={textData.header1}/>
+      <Button text={textData.badText} handleClick={badIncrease} />
+      <Button text={textData.neutralText} handleClick={neutralIncrease} />
+      <Button text={textData.goodText} handleClick={goodIncrease} />
+      <Statistics texts={textData} values={{"bad":badValue, "neutral":neutralValue, "good":goodValue, "average":averageValue}}/>
+    </div>
   )
 }
 
