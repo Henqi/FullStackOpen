5aa1cef0d6156f442a9af6ea27e3a5d4b8af1775
Henri Heilala <henri.heilala@nitor.com>
2023-01-30 10:15:17 +0200

    complete 1.7

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -28,26 +28,38 @@ const App = (props) => {
   const [badValue, setBadValue] = useState(0)
   const [neutralValue, setNeutralValue] = useState(0)
   const [goodValue, setGoodValue] = useState(0)
+  const [averageValue, setAverageValue] = useState(0)
 
   const header1 = "Give Feedback!"
   const header2 = "Statistics"
   const badText = "Bad"
   const neutralText = "Neutral"
   const goodText = "Good! :)"
+  const averageText = "Average of reviews"
+  const allText = "All"
+  const positiveText = "Positive"
 
   const badIncrease = () => {
     console.log("increasing bad value by 1")
     setBadValue(badValue+1)
+    averageCalc()
   }
   
   const neutralIncrease = () => {
     console.log("increasing neutral value by 1")
     setNeutralValue(neutralValue+1)
+    averageCalc()
   }
   
   const goodIncrease = () => {
     console.log("increasing good value by 1")
     setGoodValue(goodValue+1)
+    averageCalc()
+  }
+
+  const averageCalc = () => {
+    console.log("calculating average review value")
+    setAverageValue([badValue*-1, neutralValue*0, goodValue*1].reduce((average, current) => average += current)/(badValue+neutralValue+goodValue))
   }
 
   return (
@@ -60,6 +72,9 @@ const App = (props) => {
       <StatRow text={badText} interactions={badValue}/>
       <StatRow text={neutralText} interactions={neutralValue}/>
       <StatRow text={goodText} interactions={goodValue}/>
+      <StatRow text={allText} interactions={badValue+neutralValue+goodValue}/>
+      <StatRow text={averageText} interactions={averageValue}/>
+      <StatRow text={positiveText} interactions={(goodValue/(badValue+neutralValue+goodValue)*100)+'%'}/>
     </>
   )
 }
