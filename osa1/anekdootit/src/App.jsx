e4716b58b5f3f031b30e451696a9536eac8d16a9
Henri Heilala <henri.heilala@nitor.com>
2023-01-31 12:49:24 +0200

    complete 1.12

---
+++ osa1/anekdootit/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+
+const Button = ({ text, handleClick }) => {
+  return(
+    <button onClick={handleClick}>    
+      {text}  
+    </button>
+  )
+}
+
+const App = () => {
+
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0) 
+
+  const selectRandom = () => {
+    setSelected(Math.floor(Math.random()*anecdotes.length))
+    console.log('Random seed is: ', selected)
+  }
+
+  return (
+    <>
+    <div>
+      {anecdotes[selected]}
+    </div>
+      <div>
+        <Button text="Press me!" handleClick={selectRandom}/>
+      </div>
+    </>
+  )
+}
+
+export default App
