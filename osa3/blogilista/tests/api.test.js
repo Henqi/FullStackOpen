417fe037b72b2dadf2d0fa2dd45fbdec878cbda5
Henri Heilala <henri.heilala@nitor.com>
2023-03-01 10:20:09 +0200

    refactor: rename test files

--- osa3/blogilista/tests/api.test.js
+++ osa3/blogilista/tests/api.test.js
@@ -4,7 +4,7 @@ const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
-const testHelper = require('../tests/test_helper')
+const testHelper = require('./test_helper')
 
 beforeEach(async () => {
   await mongoose.connection.close()
