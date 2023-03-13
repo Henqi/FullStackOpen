6cdcc98fcd2524aa178c4773ea3b41bb0fa535aa
Henri Heilala <henri.heilala@nitor.com>
2023-03-13 10:54:19 +0200

    complete 5.7 & 5.8 (add button to display blog full data & add styles)

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,55 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+
+  const [showFull, setShowFull] = useState(false)
+
+  const toggleViewBlog = () => {
+    setShowFull(!showFull)
+  }
+
+  const handleAddLike = () => {
+
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 2,
+    marginBottom: 5
+  }
+
+  if (showFull) {
+    return (
+      <div className='blogStyle' style={blogStyle}>
+        {blog.title} - {blog.author}
+        <button onClick={toggleViewBlog}>
+          hide
+        </button>
+        <br/>
+        {blog.url}
+        <br/>
+        likes: {blog.likes}
+        <button onClick={handleAddLike}>
+          like
+        </button>
+        <br/>
+        {blog.user.name}
+      </div>
+    )
+  }
+
+  else {
+    return (
+      <div className='blogStyle' style={blogStyle}>
+        {blog.title} - {blog.author}
+        <button onClick={toggleViewBlog}>
+          view
+        </button>
+      </div>
+    )
+  }
+}
 
 export default Blog
