35b5aa047214f88c3cac38127940874ff1da1d8a
Henri Heilala <henri.heilala@nitor.com>
2023-03-14 13:07:56 +0200

    update eslint configs

--- osa5/bloglist-frontend/src/.eslintrc.js
+++ osa5/bloglist-frontend/src/.eslintrc.js
@@ -1,26 +1,36 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es2021": true,
-    "node": true
+  'env': {
+    'browser': true,
+    'es6': true,
+    'jest/globals': true
   },
-  "extends": [
-    "eslint:recommended",
-    "plugin:react/recommended"
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended'
   ],
-  "overrides": [
-  ],
-  "parserOptions": {
-    "ecmaVersion": "latest",
-    "sourceType": "module"
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
+    },
+    'ecmaVersion': 2018,
+    'sourceType': 'module'
   },
-  "plugins": [
-    "react"
-  ],
-  "rules": {
+  'rules': {
     'indent': [
       'error',
       2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
     ],
     'eqeqeq': 'error',
     'no-trailing-spaces': 'error',
@@ -31,8 +41,12 @@ module.exports = {
       'error', { 'before': true, 'after': true }
     ],
     'no-console': 0,
-    "react/prop-types": 0,
-    "react/react-in-jsx-scope": 0,
-    "no-unused-vars":0
+    'react/prop-types': 0,
+    'react/react-in-jsx-scope': 'off'
+  },
+  'settings': {
+    'react': {
+      'version': 'detect'
+    }
   }
 }
