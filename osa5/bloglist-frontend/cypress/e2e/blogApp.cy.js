aac2a83bb8105fe0c0847e3f55b620a74579fba8
Henri Heilala <henri.heilala@nitor.com>
2023-03-17 11:24:57 +0200

    complete 5.18 (add cypress tests for login)

--- osa5/bloglist-frontend/cypress/e2e/blogApp.cy.js
+++ osa5/bloglist-frontend/cypress/e2e/blogApp.cy.js
@@ -18,3 +18,34 @@ describe('Blog app', function() {
   })
   
 })
+
+describe('Login', function() {
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      'username': 'Kalle', 
+      'name': 'Kalle Ankka',
+      'password': 'siideri'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users', user)
+    cy.visit('http://localhost:3000')
+  })
+
+  it('Login succeeds with correct credentials', function() {
+    cy.contains('Log in to blog app:')
+    cy.get('#login-button').click()
+    cy.get('#username').type('Kalle')
+    cy.get('#password').type('siideri')
+    cy.get('#login-button').click()
+  })
+  
+  it('Login fails with incorrect credentials', function() {
+    cy.contains('Log in to blog app:')
+    cy.get('#login-button').click()
+    cy.get('#username').type('Kalle')
+    cy.get('#password').type('olut')
+    cy.get('#login-button').click()
+
+  })
+  
+})
