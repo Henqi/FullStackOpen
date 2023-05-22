2459dc4c025d0133ff7bc9b303a53fb75cb77436
Henri Heilala <henri.heilala@nitor.com>
2023-05-22 15:50:07 +0300

    complete 6.3

---
+++ osa6/redux-anecdotes/src/components/Anecdotes.js
@@ -0,0 +1,31 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { addVote } from '../reducers/anecdoteReducer'
+
+const Anecdotes = () => {
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  const vote = (id) => {
+    dispatch(addVote(id))
+  }
+
+  return (
+    <>
+      <h2>Anecdotes</h2>
+        {anecdotes.map(anecdote =>
+          <div key={anecdote.id}>
+            <div>
+              {anecdote.content}
+            </div>
+            <div>
+              has {anecdote.votes}
+              <button onClick={() => vote(anecdote.id)}>vote</button>
+            </div>
+          </div>
+        )}
+    </>
+  )
+
+}
+
+export default Anecdotes
---
+++ osa6/redux-anecdotes/src/components/NewAnecdote.js
@@ -0,0 +1,14 @@
+
+const NewAnecdote = () => {
+    return (       
+        <>
+            <h2>create new</h2>
+            <form>
+            <div><input /></div>
+            <button>create</button>
+            </form>
+        </>
+    )
+}
+
+export default NewAnecdote
