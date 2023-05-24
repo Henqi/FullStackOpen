8c8a7230f6a235b3871ea4dfbe45c6175297137e
Henri Heilala <henri.heilala@nitor.com>
2023-05-24 16:09:59 +0300

    complete 6.14 (fetch anecdotes from json server)

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -1,9 +1,23 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
+import anecdoteService from './services/anecdotes'
+import { setAnecdotes } from './reducers/anecdoteReducer'
 
 const App = () => {
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    anecdoteService
+      .getAll()
+      .then(anecdotes => {
+        dispatch(setAnecdotes(anecdotes))
+      })
+  }, [])
 
   return (
     <div>
--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,13 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
+const anecdotesAtStart = []
 
 const getId = () => (100000 * Math.random()).toFixed(0)
 
@@ -41,9 +34,20 @@ const anecdoteSlice = createSlice({
         votes: selectedAnecdote.votes+1
       }
       return state.map(anecdote => anecdote.id === id ? updatedAnecdote : anecdote)
+    },
+    appendAnecdote(state, action) {
+      state.push(action.payload)
+    },
+    setAnecdotes(state, action) {
+      return action.payload
     }
   }
 })
 
-export const { createAnecdote, voteAnecdote } = anecdoteSlice.actions
+export const {
+  createAnecdote,
+  voteAnecdote,
+  appendAnecdote,
+  setAnecdotes
+} = anecdoteSlice.actions
 export default anecdoteSlice.reducer
